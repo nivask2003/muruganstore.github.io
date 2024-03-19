74dd96af9c8b8376f0b1047fff6781b6d1a4fcd2
nivask2003 <163018894+nivask2003@users.noreply.github.com>
2024-03-19 20:30:27 +0530

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,1 +1,7 @@
-
+<?php
+session_start();
+include "Includes/function.php";
+$pdo = pdo_connet();
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'login';
+include $page . '.php';
+?>
