42a65c564017cc78446862748a4a63249d99b613
nivask2003 <163018894+nivask2003@users.noreply.github.com>
2024-03-19 22:27:21 +0530

    Create placeorder.php

---
+++ placeorder.php
@@ -0,0 +1,13 @@
+<?php
+include "Includes/header.php";
+unset($_SESSION['cart']);
+?>
+    <main>
+        <div class="placeorder container mt-3">
+           <h1 style="color:blue">Your Order placed</h1>
+           <p style="color:gray">Futher information send through your mail</p> 
+        </div>
+    </main>
+<?php
+include "Includes/footer.php";
+?>                        
