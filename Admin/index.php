a7947e75352bce397e50d845ecc3cb37f717c721
nivask2003 <163018894+nivask2003@users.noreply.github.com>
2024-03-19 22:51:52 +0530

    Delete Admin/index.php

--- Admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include "Includes/function.php";
-$pdo = pdo_connet();
-$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'login';
-include $page . '.php';
-?>
